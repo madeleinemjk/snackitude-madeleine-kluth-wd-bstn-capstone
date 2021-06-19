be5fd32aec8fbe10348778b726f0f5583a1d34c5
Madeleine Kluth <madeleinekluth@Madeleines-MacBook-Pro.local>
2021-06-19 18:18:47 +0100

    Install dependencies

---
+++ Server/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const db = require('./models');
+const restaurantRoutes = require('./routes/restaurants');
+const orderRoutes = require('./routes/orders');
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:2469",
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+});
+
+app.use((req, _, next) => {
+    req.io = io;
+    next();
+});
+
+require('dotenv').config();
+const PORT = process.env.PORT || 2468;
+
+app.use(express.json());
+app.use(cors());
+
+
+
+// NB: RELOAD_DB will drop and re-create tables, losing data
+const reloadDb = process.env.RELOAD_DB === 'true';
+console.log(`Reload DB set to ${reloadDb}`);
+db.sequelize.sync({force: reloadDb})
+    .then(() => {
+        console.log('Db sync completed');
+    });
+
+server.listen(PORT, () => {
+    console.log(`Express server listening on port ${PORT}`);
+}); 
