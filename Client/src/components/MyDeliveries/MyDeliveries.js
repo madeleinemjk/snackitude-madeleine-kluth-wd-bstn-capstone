78794470c83a28408ceeb05e4dc3984ca86b0587
Madeleine Kluth <madeleinekluth@Madeleines-MacBook-Pro.local>
2021-06-28 19:10:05 +0100

    MyRequests and MyDeliveries tables and desktop styles

--- Client/src/components/MyDeliveries/MyDeliveries.js
+++ Client/src/components/MyDeliveries/MyDeliveries.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import "./MyDeliveries.scss";
 import {getToken} from "../../utils/auth";
+import {Link} from "react-router-dom";
 
 const API_URL = process.env.REACT_APP_BACKEND_URL;
 
-const getMyDeliveriesRoute = (deliveringUserId) => {
-    return `${API_URL}/delivering/${deliveringUserId}`;
+const getMyDeliveriesRoute = () => {
+    return `${API_URL}/snack-requests/delivering`;
 };
-
 
 export default class MyDeliveries extends Component {
     constructor(props) {
@@ -24,10 +25,9 @@ export default class MyDeliveries extends Component {
     }
 
     fetchData = () => {
-        const userId = this.props.match.params.deliveringUserId;
         const token = getToken();
 
-        axios.get(getMyDeliveriesRoute(userId), {
+        axios.get(getMyDeliveriesRoute(), {
             headers: {
                 "Authorization": token
             }
@@ -51,10 +51,39 @@ export default class MyDeliveries extends Component {
         }
     };
 
+    viewRequest = (request) => {
+
+    };
+
     render() {
         return (
-            <div>
-                
+            <div className="my-deliveries">
+                <h1>My Snack Deliveries</h1>
+                {this.state.myDeliveries?.length ?
+                    <div className="table">
+                        <div className="table-header">
+                            <div className="col">Description</div>
+                            <div className="col">Max Wait Time</div>
+                            <div className="col">Budget</div>
+                            <div className="col">Address Name</div>
+                            <div className="col">Action</div>
+                        </div>
+                        <div className="table-body">
+                            {this.state.myDeliveries.map(request => {
+                                return <div className="table-row" key={request.id}>
+                                    <div className="col">{request.description}</div>
+                                    <div className="col">{request.maxWaitTime}</div>
+                                    <div className="col">{request.budget}</div>
+                                    <div className="col">{request.addressName}</div>
+                                    <div className="col">
+                                        <Link to={`/requests/${request.id}`}><button>View</button></Link>
+                                    </div>
+                                </div>
+                            })}
+                        </div>
+                    </div> :
+                    <p>You have not accepted any snack requests yet</p>
+                }
             </div>
         )
     }
--- Client/src/components/MyRequests/MyRequests.js
+++ Client/src/components/MyRequests/MyRequests.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import "./MyRequests.scss";
 import {getToken} from "../../utils/auth";
+import {Link} from "react-router-dom";
 
 const API_URL = process.env.REACT_APP_BACKEND_URL;
 
-const getMyRequestsRoute = (requestingUserId) => {
-    return `${API_URL}/requesting/${requestingUserId}`;
+const getMyRequestsRoute = () => {
+    return `${API_URL}/snack-requests/requesting`;
 };
 
 export default class MyRequests extends Component {
@@ -23,10 +25,9 @@ export default class MyRequests extends Component {
     }
 
     fetchData = () => {
-        const userId = this.props.match.params.requestingUserId;
         const token = getToken();
 
-        axios.get(getMyRequestsRoute(userId), {
+        axios.get(getMyRequestsRoute(), {
             headers: {
                 "Authorization": token
             }
@@ -51,8 +52,36 @@ export default class MyRequests extends Component {
 
     render() {
         return (
-            <div>
-                
+            <div className="my-requests">
+                <div class="top-row">
+                    <h1>My Snack Requests</h1>
+                    <Link to="/create"><button>Create Snack Request</button></Link>
+                </div>
+                {this.state.myRequests?.length ?
+                    <div className="table">
+                        <div className="table-header">
+                            <div className="col">Description</div>
+                            <div className="col">Max Wait Time</div>
+                            <div className="col">Budget</div>
+                            <div className="col">Address Name</div>
+                            <div className="col">Action</div>
+                        </div>
+                        <div className="table-body">
+                            {this.state.myRequests.map(request => {
+                                return <div className="table-row" key={request.id}>
+                                    <div className="col">{request.description}</div>
+                                    <div className="col">{request.maxWaitTime}</div>
+                                    <div className="col">{request.budget}</div>
+                                    <div className="col">{request.addressName}</div>
+                                    <div className="col">
+                                        <Link to={`/requests/${request.id}`}><button>View</button></Link>
+                                    </div>
+                                </div>
+                            })}
+                        </div>
+                    </div> :
+                    <p>You have not created any snack requests yet</p>
+                }
             </div>
         )
     }
