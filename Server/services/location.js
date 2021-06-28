745b0e894c774d08c2f7c9fa81953b2a8ce4dec7
Madeleine Kluth <madeleinekluth@Madeleines-MacBook-Pro.local>
2021-06-28 20:01:13 +0100

    First tidy

--- Server/services/location.js
+++ Server/services/location.js
@@ -1,10 +1,9 @@
 const {PlaceInputType} = require("@googlemaps/google-maps-services-js");
 const {Client} = require("@googlemaps/google-maps-services-js");
 const client = new Client({});
-// const API_KEY = 'AIzaSyBy4gPCzlxkg1_hOzj_HXz06BBbz05tdbc';
 const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
-// Find places from a string input (e.g. 4 Station Road, POST COD)
+// Find places from a string input (e.g. 47 Station Road, PG18 9PG)
 const findPlaceFromText = async (input) => {
     const places = await client.findPlaceFromText({
         params: {
