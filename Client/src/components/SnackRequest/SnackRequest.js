2dfb9fdf022d1a1d2d3bcf9686a016f965b529ea
Madeleine Kluth <madeleinekluth@Madeleines-MacBook-Pro.local>
2021-06-28 19:32:54 +0100

    SnackRequest updated and desktop styles

--- Client/src/components/SnackRequest/SnackRequest.js
+++ Client/src/components/SnackRequest/SnackRequest.js
@@ -4,8 +4,10 @@ import axios from 'axios';
 import {getToken} from "../../utils/auth";
 import socketIOClient from "socket.io-client";
 import {Link} from "react-router-dom";
+import {toast} from "react-toastify";
 
 const API_URL = process.env.REACT_APP_BACKEND_URL;
+const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
 const getSnackRequestRoute = (id) => {
     return `${API_URL}/snack-requests/${id}`;
@@ -51,6 +53,7 @@ class SnackRequest extends Component {
                 "Authorization": token
             }
         }).then(res => {
+            console.log('Got new data');
             this.setState({
                 snackRequest: res.data
             })
@@ -89,28 +92,47 @@ class SnackRequest extends Component {
             headers: {
                 "Authorization": token
             }
-        })
+        }).then(res => {
+            console.log(res.data);
+            toast.info('Successfully added message!');
+        }).catch(err => {
+            toast.error('Could not add message');
+        });
     };
 
     render() {
         return <div className="snack-request">
-            <h1>Snack Request Page</h1>
+            <h1>Snack Request {this.state.snackRequest?.id}</h1>
             <p>Description: {this.state.snackRequest?.description}</p>
             <p>Budget: {this.state.snackRequest?.budget}</p>
-            <p>MaxWaitTime: {this.state.snackRequest?.maxWaitTime}</p>
-            <p>Address Name: {this.state.snackRequest?.addressName}</p>
-            <p>Delivered By: {this.state.snackRequest?.deliveringUser?.firstName} -
-                <Link to={`/user/${this.state.snackRequest?.deliveringUserId}`}>Review</Link>
-            </p>
-            <p>Requested By: {this.state.snackRequest?.requestingUser?.firstName} -
-            <Link to={`/user/${this.state.snackRequest?.requestingUserId}`}>Review</Link>
-            </p>
+            <p>Wait Time: {this.state.snackRequest?.maxWaitTime}</p>
+            <p>Address: {this.state.snackRequest?.addressName}</p>
+            {this.state.snackRequest?.deliveringUser?.firstName ?
+                <p>Delivered By: <Link to={`/user/${this.state.snackRequest?.deliveringUserId}`}>{this.state.snackRequest?.deliveringUser?.firstName}</Link> - from {this.state.snackRequest.fromAddressName}</p>
+                : null}
+            <p>Duration: {Math.ceil(this.state.snackRequest?.duration / 60)} min</p>
+            <p>Distance: {(this.state.snackRequest?.distance / 1000)}km</p>
+            <p>Requested By: <Link to={`/user/${this.state.snackRequest?.requestingUserId}`}>{this.state.snackRequest?.requestingUser?.firstName}</Link></p>
+            {this.props.user?.id === this.state.snackRequest?.deliveringUserId ?
+                <div className="directions">
+                    <h2>Directions</h2>
+                    <iframe title="Delivery Directions"
+                        width="450"
+                        height="250"
+                        frameBorder="0"
+                        src={`https://www.google.com/maps/embed/v1/directions?key=${API_KEY}&origin=place_id:${this.state.snackRequest?.fromPlaceId}&destination=place_id:${this.state.snackRequest?.placeId}`} allowFullScreen>
+                    </iframe>
+                </div>
+                : null}
             <h2>Messages</h2>
-            {this.state.snackRequest?.messages?.map(message => {
-                return <p>{message?.message} - {message?.sendingUser?.firstName}</p>
-            })};
-            <textarea onChange={this.handleMessageChange}></textarea>
-            <button onClick={this.handleMessageSubmit}>Add Message</button>
+            {this.state.snackRequest?.messages?.length ? this.state.snackRequest?.messages?.map(message => {
+                return <div className="message" key={message.id}>
+                    <p className="message-content">{message?.message}</p>
+                    <p className="message-sender">{message?.sendingUser?.firstName}</p>
+                </div>
+            }) : <p>There are no messages yet. You can send one below!</p>}
+            <textarea name="message" id="message" placeholder="Enter message text" onChange={this.handleMessageChange} />
+            <button onClick={this.handleMessageSubmit}>Send Message</button>
         </div>
     }
 }
